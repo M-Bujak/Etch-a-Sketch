7a102549f4d614612f1f4312ac77210f0fd43ad1
Mateusz Bujak <62397807+M-Bujak@users.noreply.github.com>
2022-09-18 23:51:15 +0200

    Code formatting

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 const gridContainer = document.getElementById('grid-container');
 
-function initializeGrid(horizontalCellCount, verticalCellCount){
-    for(let vert = 0; vert < verticalCellCount; vert++)
-    {
-        for(let hor = 0; hor < horizontalCellCount; hor++)
-        {
+function initializeGrid(horizontalCellCount, verticalCellCount) {
+    for (let vert = 0; vert < verticalCellCount; vert++) {
+        for (let hor = 0; hor < horizontalCellCount; hor++) {
             let newCell = document.createElement('div');
             newCell.classList.add('grid-item');
             newCell.dataset.x = hor;
@@ -16,17 +14,17 @@ function initializeGrid(horizontalCellCount, verticalCellCount){
     setNumberOfRowsAndColumns(horizontalCellCount, verticalCellCount);
 }
 
-function setNumberOfRowsAndColumns(horizontalCellCount, verticalCellCount){
+function setNumberOfRowsAndColumns(horizontalCellCount, verticalCellCount) {
     let styleTag = 'grid-template-columns: ';
-    for(let i = 0; i < horizontalCellCount; i++){
+    for (let i = 0; i < horizontalCellCount; i++) {
         styleTag += ' auto';
     }
 
     styleTag += '; grid-template-rows: ';
-    for(let i = 0; i < verticalCellCount; i++){
+    for (let i = 0; i < verticalCellCount; i++) {
         styleTag += ' auto';
     }
     gridContainer.style.cssText = styleTag + ';';
 }
 
-initializeGrid(16,16);
+initializeGrid(16, 16);
