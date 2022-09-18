334f1164296b07b8d93c9c0e99359fa0e9f5a0cf
Mateusz Bujak <62397807+M-Bujak@users.noreply.github.com>
2022-09-18 21:18:19 +0200

    set basic structure for solution

---
+++ script.js
@@ -0,0 +1,11 @@
+const gridContainer = document.getElementsById('grid-container');
+
+function initializeGrid(horizontalCellCount, verticalCellCount){
+    for(let vert = 0; vert < verticalCellCount; vert++)
+    {
+        for(let hor = 0; hor < horizontalCellCount; hor++)
+        {
+            
+        }
+    }
+}
